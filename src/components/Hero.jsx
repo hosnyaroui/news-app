e4dc6863a7d9670d6fbc7c6da434836591de93df
hosnyaroui <hosny.cris@gmail.com>
2024-02-01 15:14:29 +0100

    adding hero section

---
+++ src/components/Hero.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div>
+      <header>
+        <div className='hero'>
+<div className='hero-banner'>
+<h4>News Store</h4>
+<p>All new news is an old news happening to new people</p>
+<input type='text' placeholder='' className='input-bar' />
+<button className='hero-btn'>search</button>
+        </div>
+        </div>
+      </header>
+    </div>
+  )
+}
+
+export default Hero
