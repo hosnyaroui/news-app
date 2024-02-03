b304d276ab2268633e39f7b00fa7967a7621e543
hosnyaroui <hosny.cris@gmail.com>
2024-02-03 11:05:28 +0100

    creating news cards

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@
 import './App.css';
 import Hero from './components/Hero';
 import '../src/index.css'
+import Main from './components/Main';
 
 function App() {
   return (
     <div className="App">
       <Hero />
+      <Main />
     </div>
   );
 }
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -9,15 +9,33 @@ getNews();
 
 
 const getNews = async () => {
-        const api = await fetch("https://newsapi.org/v2/top-headlines?q=general&apiKey=API_KEY");
+        const api = await fetch("https://newsapi.org/v2/top-headlines?q=general&apiKey=2d79f42db6694d78a9b08c7e12462bfc");
         const data = await api.json();
         setNews(data.articles);
 console.log(data.articles);
     }
 
   return (
-    <div>
-      
+    <div className='news'>
+      <div className='row'>
+{
+  news.map((item) => (
+    <div className='col col-lg-4 col-md-6 col-sm-12'>
+<div className='card'>
+<img src={item.urlToImage} alt="" className='card-img-top' />
+<h6 className='card-header'>{item.title}</h6>
+<div className='card-body'>
+  <p>{item.description}</p>
+  <button className='btn btn-success'>
+    <a href={item.url}>More</a>
+  </button>
+</div>
+</div>
+    </div>
+  ))
+}
+
+      </div>
     </div>
   )
 }
