9ee1b6c024bfa6732e65a13b51c0e224fe74cc55
hosnyaroui <hosny.cris@gmail.com>
2024-02-04 16:50:35 +0100

    styling cards

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -14,18 +14,24 @@ const getNews = async () => {
         setNews(data.articles);
 console.log(data.articles);
     }
+const img = "https://c0.wallpaperflare.com/preview/105/94/569/administration-articles-bank-black-and-white.jpg"
+
+const setImage = event => {
+  event.target.src = img
+}
 
   return (
     <div className='news'>
       <div className='row'>
 {
   news.map((item) => (
-    <div className='col col-lg-4 col-md-6 col-sm-12'>
+    <div className='col col-lg-4 col-md-6 col-sm-12 news-column'>
 <div className='card'>
-<img src={item.urlToImage} alt="" className='card-img-top' />
+
+<img src={item.urlToImage} alt="" className='card-img-top' onError={setImage} />
 <h6 className='card-header'>{item.title}</h6>
 <div className='card-body'>
-  <p>{item.description}</p>
+  <p className='news-text' style={{fontSize: 14}}>{item.description}</p>
   <button className='btn btn-success'>
     <a href={item.url}>More</a>
   </button>
